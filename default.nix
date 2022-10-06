863ca907614ddaa3442864fe9c7a84fece81c60c
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2022-10-06 16:55:39 +0200

    fixup! Update mutwo.core in nix

--- default.nix
+++ default.nix
@@ -4,7 +4,7 @@ with pkgs.python3Packages;
 
 let
 
-  mutwo-core-archive = builtins.fetchTarball "https://github.com/mutwo-org/mutwo.core/archive/97aea97f996973955889630c437ceaea405ea0a7.gz";
+  mutwo-core-archive = builtins.fetchTarball "https://github.com/mutwo-org/mutwo.core/archive/97aea97f996973955889630c437ceaea405ea0a7.tar.gz";
   mutwo-core = import (mutwo-core-archive + "/default.nix");
 
   treelib = pkgs.python39Packages.buildPythonPackage rec {
@@ -27,8 +27,11 @@ let
       rev = "38ac789b61e1e33d1a8be999ca969f909bb652c0";
       sha256 = "sha256-oRQCtDBQnViNP6sJZU0NqFWkn2YpcIeGWmfx3Ne/n2c=";
     };
+    # TypeError: calling <class 'ranges.RangeDict.RangeDict'> returned {}, not a test
     doCheck = false;
+    propagatedBuildInputs = [ python39Packages.pytest ];
   };
+
 
 in
 
