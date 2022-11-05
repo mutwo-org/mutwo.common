346f8ccbd85ce58904cd62f6f9a16ca08436736f
levinericzimmermann <levinericzimmermann@posteo.eu>
2022-11-06 00:53:36 +0100

    Update nix derivation

--- default.nix
+++ default.nix
@@ -4,7 +4,7 @@ with pkgs.python310Packages;
 
 let
 
-  mutwo-core-archive = builtins.fetchTarball "https://github.com/mutwo-org/mutwo.core/archive/069a407e5ac1bd140d92180e21e64dd094df83c5.tar.gz";
+  mutwo-core-archive = builtins.fetchTarball "https://github.com/mutwo-org/mutwo.core/archive/28a13e348876fa07929f5fd4f3953fee624c255c.tar.gz";
   mutwo-core = import (mutwo-core-archive + "/default.nix");
 
   treelib = pkgs.python310Packages.buildPythonPackage rec {
@@ -42,6 +42,9 @@ in
       rev = "dd8b4e5355cb0bacb0085146f975e620a6384abe";
       sha256 = "sha256-L0ruY6ehQFwXnxqq5iY+DF9RTURcI5E+e6NGMFULEoQ=";
     };
+    checkInputs = [
+      python310Packages.pytest
+    ];
     propagatedBuildInputs = [ 
       treelib
       python310Packages.numpy
@@ -49,5 +52,10 @@ in
       python-ranges
       mutwo-core
     ];
+    checkPhase = ''
+      runHook preCheck
+      pytest
+      runHook postCheck
+    '';
     doCheck = true;
   }
