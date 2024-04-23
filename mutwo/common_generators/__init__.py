62cf1b1bcb3968afeb5a450195733f9fc7594409
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2024-04-23 22:15:09 +0200

    generators += DynamicChoice
    'DynamicChoice' resided in 'mutwo.core' before v.2.0.0: https://github.com/mutwo-org/mutwo.core/commit/974aac303ed0c800b570b8c5d094137d06b0dbb4
    As it was dropped there, we can re-add it here.

--- mutwo/common_generators/__init__.py
+++ mutwo/common_generators/__init__.py
@@ -4,6 +4,7 @@ from .brown import *
 from .brun import *
 from .chomksy import *
 from .edwards import *
+from .generic import *
 from .gray import *
 from .koenig import *
 from .lehmer import *
@@ -12,8 +13,8 @@ from .toussaint import *
 from mutwo import core_utilities
 
 __all__ = core_utilities.get_all(
-    brown, brun, chomksy, edwards, gray, koenig, lehmer, toussaint
+    brown, brun, chomksy, edwards, generic, gray, koenig, lehmer, toussaint
 )
 
 # Force flat structure
-del brown, brun, core_utilities, chomksy, edwards, gray, koenig, lehmer, toussaint
+del brown, brun, core_utilities, chomksy, edwards, generic, gray, koenig, lehmer, toussaint
